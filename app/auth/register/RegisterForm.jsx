fff18f13deed9ac91bdc1a178ee93e7b433e67c6
Tony Ligogo <tonyligogo@gmail.com>
2025-05-07 15:30:18 +0300

    auth forms

---
+++ app/auth/register/RegisterForm.jsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useForm } from "react-hook-form"
+import Link from "next/link";
+
+export function RegisterForm({
+  className,
+  ...props
+}) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm()
+    
+      const onSubmit = (data) => console.log(data)
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Sign up</CardTitle>
+          <CardDescription>
+            Enter your credentials to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="username"
+                  {...register("username", { required: 'Username is required' })}
+                  />
+                  { errors.username && <span className="text-red-500">This field is required</span> }
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  {...register("email", { required: 'Email is required' })}
+                  />
+                  { errors.email && <span className="text-red-500">This field is required</span> }
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password"  {...register("password", { required: 'Password is required', minLength:{value:5, message:'Password must be at least 5 characters long'} })} />
+                {errors.password && <span className="text-red-500">{errors.password.message}</span> }
+              </div>
+              <Button type="submit" className="w-full">
+                Register
+              </Button>
+              <Button variant="outline" className="w-full">
+                Register with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link href="/auth/login" className="underline underline-offset-4">
+                Sign in
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { RegisterForm } from './RegisterForm'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+          <div className="w-full max-w-sm">
+            <RegisterForm />
+          </div>
+        </div>
   )
 }
 
